1f952ab3eba9ccdd889354d115a9e46b8a8415b6
Rohit Parab <rohitparab213@gmail.com>
2021-05-05 04:53:36 +0530

    Added function for prev and next functionality

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ document.getElementById("carousel-button-next").addEventListener("click",moveToN
 document.getElementById("carousel-button-prev").addEventListener("click",moveToPrevSlide)
 
 function moveToNextSlide(){
-
+    if(slidePosition === totalSlides-1){
+        slidePosition=0;
+    }
+    else{
+        slidePosition++;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
 
 function moveToPrevSlide(){
